67c0712271910cb935fcf5280ae2f86f380f1baf
Mounika530Sheri <sherimounika100@gmail.com>
2025-09-21 15:36:24 +0530

    Update user.js

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,43 +1,65 @@
-const User=require("../model/user")
-const bcrypt=require("bcrypt")
-const jwt=require("jsonwebtoken")
+const User = require("../model/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-
-const userSignup=async(req,res)=>{
-    const {email,password}=req.body
-    if(!email || !password){
-        return res.json({message:"fields cann't be empty"}).status(400)
-    }
-    let existuser=await User.findOne({email})
-    if(existuser){
-        return res.json({message:"already Exists"}).status(400)
-    }
-    const hashpwd=await bcrypt.hash(password,10)
-    const newuser=await User.create({email,password:hashpwd})
-    let token=jwt.sign({email,id:newuser._id},process.env.SECRETE_KEY)
-    return res.json({token,user:newuser}).status(200)
-}
-
-const userLogin=async(req,res)=>{
-    const {email,password}=req.body
-    if(!email || !password){
-        return res.json({message:"fields cann't be empty"}).status(400)
-    }
-    let existuser=await User.findOne({email})
-    if(existuser && await bcrypt.compare(password,existuser.password)){
-        let token=jwt.sign({email,id:existuser._id},process.env.SECRETE_KEY)
-        return res.json({token,user:existuser}).status(200)
-
-    }else{
-         return res.status(400).json({ error: "Invaild credientials" })
+const userSignup = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Fields can't be empty" });
     }
 
+    const existuser = await User.findOne({ email });
+    if (existuser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
 
-}
+    const hashpwd = await bcrypt.hash(password, 10);
+    const newuser = await User.create({ email, password: hashpwd });
 
-const getUser=async(req,res)=>{
-    const user=await User.findById(req.params.id)
-    return res.json({email:user.email})
-}
+    const token = jwt.sign({ email, id: newuser._id }, process.env.SECRETE_KEY, {
+      expiresIn: "1h",
+    });
 
-module.exports={userSignup,userLogin,getUser}
+    return res.status(201).json({ token, user: newuser });
+  } catch (error) {
+    console.error("Signup Error:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Fields can't be empty" });
+    }
+
+    const existuser = await User.findOne({ email });
+    if (existuser && (await bcrypt.compare(password, existuser.password))) {
+      const token = jwt.sign(
+        { email, id: existuser._id },
+        process.env.SECRETE_KEY,
+        { expiresIn: "1h" }
+      );
+      return res.status(200).json({ token, user: existuser });
+    } else {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.error("Login Error:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    return res.json({ email: user.email });
+  } catch (error) {
+    console.error("GetUser Error:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = { userSignup, userLogin, getUser };
